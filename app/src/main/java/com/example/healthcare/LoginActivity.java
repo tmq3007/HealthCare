937cea4d6a04d03962dd943b44052ffb1f073675
Quan Truong <158136735+tmq3007@users.noreply.github.com>
2025-10-14 00:25:22 +0700

    Initial commit

---
+++ app/src/main/java/com/example/healthcare/LoginActivity.java
@@ -0,0 +1,79 @@
+package com.example.healthcare;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class LoginActivity extends AppCompatActivity {
+
+    EditText edUsername, edPassword;
+
+    TextView tv;
+
+    Button btn;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_login);
+
+        edUsername = findViewById(R.id.editTextLoginUsername);
+        edPassword = findViewById(R.id.editTextLoginPassword);
+        tv = findViewById(R.id.textViewRegister);
+        btn = findViewById(R.id.button);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String username = edUsername.getText().toString();
+                String password = edPassword.getText().toString();
+                Database db = new Database(getApplicationContext(),"healthcare",null,1);
+
+                if(username.isEmpty() || password.isEmpty()){
+                    Toast.makeText(getApplicationContext(),"Please enter your username and password",Toast.LENGTH_SHORT).show();
+                }else{
+                    if (db.login(username,password)==1){
+                        Toast.makeText(getApplicationContext(),"Login successfully",Toast.LENGTH_SHORT).show();
+
+                        SharedPreferences sharedPreferences = getSharedPreferences("shared_prefs",MODE_PRIVATE);
+                        SharedPreferences.Editor editor = sharedPreferences.edit();
+                        editor.putString("username",username);
+                        //save data with key-value
+                        editor.apply();
+
+                        startActivity(new Intent(LoginActivity.this, HomeActivity.class));
+                    }else {
+                        Toast.makeText(getApplicationContext(),"Login failed, invalid username or password",Toast.LENGTH_SHORT).show();
+                    }
+                }
+            }
+        });
+
+        tv.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(LoginActivity.this, RegisterActivity.class));
+            }
+        });
+
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+}
---
+++ app/src/main/java/com/example/healthcare/RegisterActivity.java
@@ -0,0 +1,113 @@
+package com.example.healthcare;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    EditText edUsername, edEmail, edPassword, edConfirmPassword;
+
+    TextView tv;
+
+    Button btn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_register);
+
+        edUsername = findViewById(R.id.editTextRegUsername);
+        edEmail = findViewById(R.id.editTextRegEmail);
+        edPassword = findViewById(R.id.editTextRegPassword);
+        edConfirmPassword = findViewById(R.id.editTextRegConfirmPassword);
+        tv = findViewById(R.id.textViewHaveAcc);
+        btn = findViewById(R.id.button);
+        tv.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(RegisterActivity.this,LoginActivity.class));
+            }
+        });
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String username = edUsername.getText().toString();
+                String email = edEmail.getText().toString();
+                String password = edPassword.getText().toString();
+                String confirmPassword = edConfirmPassword.getText().toString();
+
+                Database db = new Database(getApplicationContext(),"healthcare",null,1);
+
+                if(username.isEmpty() || password.isEmpty() || confirmPassword.isEmpty() || email.isEmpty()){
+                    Toast.makeText(getApplicationContext(),"Please enter your username and password",Toast.LENGTH_SHORT).show();
+                }else{
+                    if(password.compareTo(confirmPassword)==0){
+                        if (isValid(password)){
+                            //in db
+                            db.register(username,email,password);
+
+                            Toast.makeText(getApplicationContext(),"Record Inserted",Toast.LENGTH_SHORT).show();
+                            startActivity(new Intent(RegisterActivity.this, LoginActivity.class));
+                        }else {
+                            Toast.makeText(getApplicationContext(),"Password must contain at least 8 characters, having " +
+                                    "letters, numbers and special characters",Toast.LENGTH_SHORT).show();
+
+                        }
+                    }else {
+                        Toast.makeText(getApplicationContext(),"Password and confirm password does not match",Toast.LENGTH_SHORT).show();
+                    }
+                }
+            }
+        });
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+
+    public static boolean isValid(String passwordhere) {
+        int f1 = 0, f2 = 0, f3 = 0;
+
+        if (passwordhere.length() < 8) {
+            return false;
+        } else {
+            for (int p = 0; p < passwordhere.length(); p++) {
+                if (Character.isLetter(passwordhere.charAt(p))) {
+                    f1 = 1;
+                }
+            }
+
+            for (int r = 0; r < passwordhere.length(); r++) {
+                if (Character.isDigit(passwordhere.charAt(r))) {
+                    f2 = 1;
+                }
+            }
+
+            for (int s = 0; s < passwordhere.length(); s++) {
+                char c = passwordhere.charAt(s);
+                if ((c >= 33 && c <= 46) || c == 64) {
+                    f3 = 1;
+                }
+            }
+
+            if (f1 == 1 && f2 == 1 && f3 == 1)
+                return true;
+            return false;
+        }
+    }
+
+}
