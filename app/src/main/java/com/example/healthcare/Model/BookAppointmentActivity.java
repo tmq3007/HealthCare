4588cce2909df5c2ee6008c84d3623b7632539a1
NGUYEN DUC DAT <nguyenducdat0406@gmail.com>
2025-10-23 22:02:28 +0700

    Add BookAppointmentActivity and layout
    Introduced BookAppointmentActivity with its corresponding layout for booking appointments. Updated AndroidManifest.xml to register the new activity.

---
+++ app/src/main/java/com/example/healthcare/Model/BookAppointmentActivity.java
@@ -0,0 +1,26 @@
+package com.example.healthcare.Model;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.example.healthcare.R;
+
+public class BookAppointmentActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_book_appointment);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+}
